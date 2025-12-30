9f2b44e3a9fed6486cf66842e3a5d6836421e69c
venkatasatwik20 <satwik.putchakayala@gmail.com>
2025-12-30 22:23:45 +0530

    Add files via upload

---
+++ week 1/#week-1 question 2.py
@@ -0,0 +1,6 @@
+#week-1 question 2
+year= input("enter your birth year")
+birth_year=int(year)
+age=2025-birth_year 
+print(f"You will be {age} years old")
+
---
+++ week 1/Q1.py
@@ -0,0 +1,8 @@
+name="venkata satwik"
+age=18
+height=180
+are_you_student= True 
+print("my name is"+ name)
+print("my age is "+ str(age))
+print("my height is"+ str(height))
+print("are_you_student?"+ str(are_you_student))
