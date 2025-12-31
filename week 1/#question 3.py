89543675649fd85e8308ecd858900d379d7dfe99
venkatasatwik20 <satwik.putchakayala@gmail.com>
2025-12-31 18:33:10 +0530

    Add files via upload

---
+++ week 1/#question 3.py
@@ -0,0 +1,5 @@
+#question 3
+price=599.99
+discount=0.15
+final_price = price - (price * discount)
+print(f"Final price after discount {final_price:.2f}")
---
+++ week 1/question 4.py
@@ -0,0 +1,6 @@
+#question 4
+favourite_number=16
+if favourite_number % 2 == 0:
+    print("it is an Even number")
+else:
+    print("it is an Odd number")
