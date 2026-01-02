0e6496141a118d8101145fea368690c985f4b9bf
venkatasatwik20 <satwik.putchakayala@gmail.com>
2026-01-02 21:40:06 +0530

    Add files via upload

---
+++ week 1/Q7.py
@@ -0,0 +1,8 @@
+#Q7
+num = int(input("Enter the number : "))
+if(num>0):
+    print("The number is positive")
+elif(num<0):
+    print("The number is negative")
+else :
+    print("The number is 0")
