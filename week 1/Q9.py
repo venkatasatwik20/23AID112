b03a3f22ce762a3d60d6ba3656f2404d34baa9ff
venkatasatwik20 <satwik.putchakayala@gmail.com>
2026-01-02 21:44:10 +0530

    Add files via upload

---
+++ week 1/Q9.py
@@ -0,0 +1,8 @@
+#Q9
+score1 = int(input("Enter first exam score:"))
+score2 = int(input("Enter second exam score:"))
+
+if score1 >= 50 and score2 >= 50:
+    print("You passed!")
+else:
+    print("You need to retake some exams")
