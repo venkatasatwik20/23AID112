90b4ed66a1d1b447cd1046b2a41e59a42bcb787a
venkatasatwik20 <satwik.putchakayala@gmail.com>
2026-01-03 22:19:21 +0530

    Add files via upload

---
+++ week 1/Q15.py
@@ -0,0 +1,17 @@
+#Q15
+#a
+list = [12, 45, 3, 98, 7, 34, 21]
+for numbers in list:
+    print(numbers)
+#b
+list = [12, 45, 3, 98, 7, 34, 21]
+for numbers in list:
+    if numbers >30:
+       print(numbers)
+#c
+list = [12, 45, 3, 98, 7, 34, 21]
+for numbers in list:
+    c=0
+    if numbers > 30:
+        c+=1    
+print(c)       
